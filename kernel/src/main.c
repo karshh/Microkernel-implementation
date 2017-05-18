c062244cc19f210544263306976d7534a7486e72
Utkarsh Sharma <karsh.sharma@uwaterloo.ca>
2017-05-18 19:22:38 -0400

    Implemented First User Task and Other User Tasks in main.c

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,64 +1,86 @@
 #include "ts7200.h"
 
-#include "request.h"
-#include "userRequestCall.h"
 #include "bwio.h"
+#include "request.h"
+#include "queue.h"
+#include "kernelRequestCall.h"
+#include "userRequestCall.h"
 #include "interruptHandler.h"
 
+#define REDBOOT_LOAD_OFFSET 0x218000
 
-void kernelEnter(request * myRequest) {
-//changed kernal entry point to have an argument, which points to r0.
-//saves me from writing janky code
+#define USER_STACK_PTR_ADDR 0x0400000
 
-	switch(myRequest->reqType){
-	case(MYTID):
-		//#TODO change when you get create_task done
-		//lets return 314
-		myRequest->reqVal = 314;
-		//note does not change r0;
-		bwputc(COM2,'R');
-		//changes r0; but doesnt matter as we are chaning the result of 314
-		//asm("movs	pc, lr");
-		break;
-	case(MYPARENTID):
-		myRequest->reqVal = 619;
-		//#TODO change when you get create_task done
-		//lets return 69
-		break;
-	case(CREATE):
-		break;
-	case(PASS):
-		break;
-	case(EXIT):
-		break;
-	default:
-		break;
+void firstUserTask() {
+	volatile int r;
+	int priority = LOW;
+
+	int i = 1;
+	for (; i <= 4; i++) {
+
+		int r = Create(priority, (void *)otherUserTask);
+		if (r == -1) {
+			bwprintf("Invalid priority.\r\n");
+			Exit();
+		} else if (r == -2) {
+			bwprintf("Out of task descriptors.\r\n");
+			Exit();
+		} else {
+			bwprintf("Created: %d.\r\n", r);
+		}
+		if (i == 2) priority = HIGH;
 	}
 
+	bwprintf("FirstUserTask: exiting.\r\n");
+	Exit();
 
 }
 
-
-void _switch() {
-	asm ("swi 0");	
-	asm ("/* Comment */");	
+void otherUserTask() {
+	int tid = MyTid();
+	int parenttid = MyParentTid();
+	bwprintf("MyTid:%d.        MyParentTid:%d.\r\n", tid, parenttid);
 	
+	Pass();
+	
+	tid = MyTid();
+	parenttid = MyParentTid();
+	bwprintf("MyTid:%d.        MyParentTid:%d.\r\n", tid, parenttid);
+	Exit();
 }
-
-void PL() {
-	bwprintf(COM2, "CONTEXT SWITCH SUCCEEDED!");
-}
-
-static int j;
 
 int main(void) {
+	// initialization
 	bwsetfifo(COM2, OFF);
-	* ((int *) (0x28)) = ((int) kernelEnter) + 0x218000;
-	int tid = 0;
-	int ptid = 0;
-	tid = MyTid();
-	ptid = MyParentTid();
-	bwprintf(COM2, "myTid: %d", tid);
-	bwprintf(COM2, "mypTid: %d", ptid);
-	return 0;
+	* ((int *) (0x28)) = ((int) kernelEnter) + REDBOOT_LOAD_OFFSET;
+	
+	// set this to 1 to print debug statements.
+	int debug = 0;
+
+	// This variable is used to accurately set up stack of each user task.
+	// It will also be used to assign a TID for each task.
+	// It will need to be incremented for every instance of user task we make.
+	volatile int numTasks = 0;
+	queue Q;
+	queueInit(&Q);
+
+	// TODO, create first task here.
+	kernel_Create(MEDIUM, (((int)firstUserTask),
+							&numTasks, 0);
+	
+	if (debug) bwprintf(COM2, "Kernel:  Beginning run.\n\r");
+
+	TD * task;
+	request r;
+	while(queuePop(&Q, &task)) {
+		 r = *activate(task->reqVal, &(task->sp));
+		processRequest(task, &r);
+		queuePush(&Q, task, task->priority);
+	}
+
+	if (debug) bwprintf(COM2, "Kernel: Exitting.\n\r");
+
+
+
+
 }
