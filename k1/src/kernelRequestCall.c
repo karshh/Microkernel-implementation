7d9d26766f967a1bae1501329be5840e2119eb06
Paily Njarlangattil <pnjarlangattil@uwaterloo.ca>
2017-05-24 00:39:32 -0400

    moved files to k1 directory, changed README

--- k1/src/kernelRequestCall.c
+++ k1/src/kernelRequestCall.c
@@ -50,8 +50,9 @@ int kernel_Pass(TD * t) {
 
 int kernel_Exit(TD * t) {
 	//literally does absolutly nothing
-	volatile int p = ZOMBIE;
+	volatile int p = ZOMBIE_PRIORITY;
 	t->priority = (int) p;
+	t->state = ZOMBIE;
 	t->reqVal = 0;
 	return 1;
 }
@@ -59,21 +60,24 @@ int kernel_Exit(TD * t) {
 
 int kernel_Create(TD * t, request * r, kernelHandler * ks) {
 	int priority =(int) r->arg1;
-	if (priority <1 || priority >3){
+	if (priority <0 || priority >31){
 		 //change to 
 		t->reqVal = -1;
 	}else{
-		int err = getNextTID(ks,&(t->reqVal));
+		int TID  =0;
+		//int err = getNextTID(ks, &(t->reqVal));
+		int err = getNextTID(ks, &(TID));
 		//got a live child
 		if (err) 
 		{ t->reqVal  = -2;}
 		else{
-		int TID = t->reqVal;
+		t->reqVal= TID;
 		int code =(int) r->arg2;
 			
 		int PTID = t->TID;
 		TD * childTD = setTask(ks,TID, PTID,priority,code);   //if TID == , it is created by kernel
-		kernel_queuePush(ks, childTD);
+	//	kernel_queuePush(ks, childTD);
+		 kernel_queuePush(ks, childTD);
 		}
 	}
 	
