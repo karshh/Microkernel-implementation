8c058ac3ba94baec500bd3dd5efe70a53c5b0997
Paily Njarlangattil <pnjarlangattil@uwaterloo.ca>
2017-05-26 22:52:40 -0400

    adding dictionary datastruct

--- kernel/src/pkstring.c
+++ kernel/src/pkstring.c
@@ -31,3 +31,59 @@ void pkstrcpy(void *dest, const void *source) {
 	cdest[i] = 0;
 	
 }
+
+void pkstrcpy_volatile(volatile void *dest, volatile void *source) {
+	char *cdest = (char *) dest;
+	char *csource = (char *) source;
+	volatile int i = 0;
+	for (i = 0; csource[i] != 0; i++) {
+		cdest[i] = csource[i];
+	} 
+	cdest[i] = 0;
+	
+}
+
+int pkstrcmp_volatile(volatile char string1[], volatile char string2[] )
+{   volatile int i;
+    for ( i = 0; ; i++)
+    {
+        if (string1[i] != string2[i])
+        {
+            return string1[i] < string2[i] ? -1 : 1;
+        }
+
+        if (string1[i] == '\0')
+        {
+            return 0;
+        }
+    }
+}
+
+
+
+int pkstrcmp(const char string1[], const char string2[] )
+{   volatile int i;
+    for ( i = 0; ; i++)
+    {
+        if (string1[i] != string2[i])
+        {
+            return string1[i] < string2[i] ? -1 : 1;
+        }
+
+        if (string1[i] == '\0')
+        {
+            return 0;
+        }
+    }
+}
+
+
+int pkstreq(const char string1[], const char string2[] )
+{  volatile int i = pkstrcmp(string1,string2);
+    if(!i) return 1;
+    else
+    return 0;
+}
+
+
+
